d1559d3dcdce20e5a400b75a65f70fcb93115558
Abhishek Malvadkar <abhi@dev.com>
2024-03-17 11:32:22 +0530

    Initial commit

---
+++ src/test/java/com/amalvadkar/ihms/IhmsserviceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.ihms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhmsserviceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
