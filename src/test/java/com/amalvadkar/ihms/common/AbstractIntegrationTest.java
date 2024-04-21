a1863b1e90eb80163fdd46d84b464b6c07ed2ed1
Abhishek Malvadkar <abhi@dev.com>
2024-04-21 13:58:17 +0530

    GH-24 : Just refactoring

--- src/test/java/com/amalvadkar/ihms/common/AbstractIntegrationTest.java
+++ src/test/java/com/amalvadkar/ihms/common/AbstractIntegrationTest.java
@@ -1,19 +1,26 @@
 package com.amalvadkar.ihms.common;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@AutoConfigureMockMvc
 public abstract class AbstractIntegrationTest {
 
     @LocalServerPort
     int port;
+
+    static MySQLContainer<?> mysql = new MySQLContainer<>(
+            "mysql:8.0-debian"
+    );
 
     @BeforeAll
     static void beforeAll() {
@@ -25,12 +32,17 @@ public abstract class AbstractIntegrationTest {
         mysql.stop();
     }
 
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
+
+
     @BeforeEach
     void setUp(){
         RestAssured.port = port;
     }
-
-    @Autowired
-    protected MockMvc mockMvc;
 
 }
