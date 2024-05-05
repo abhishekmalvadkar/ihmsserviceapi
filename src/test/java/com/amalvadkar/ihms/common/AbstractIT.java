2a67a1671ee41042b4fde8a4e29521794f5e69e3
Abhishek Malvadkar <abhi@dev.com>
2024-05-05 20:00:37 +0530

    GH-45 : Green mail server test container setup

--- src/test/java/com/amalvadkar/ihms/common/AbstractIT.java
+++ src/test/java/com/amalvadkar/ihms/common/AbstractIT.java
@@ -1,38 +1,27 @@
 package com.amalvadkar.ihms.common;
 
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Tag("it")
 @Import(ContainersConfig.class)
-@Testcontainers
 public abstract class AbstractIT {
 
-    @Container
-    static GenericContainer<?> greenMailGenericContainer = new GenericContainer(DockerImageName.parse("greenmail/standalone:2.1.0-alpha-4"))
-            .waitingFor(Wait.forLogMessage(".*Starting GreenMail standalone.*", 1))
-            .withEnv("GREENMAIL_OPTS", "-Dgreenmail.setup.test.smtp -Dgreenmail.hostname=0.0.0.0 -Dgreenmail.users=user:admin")
-            .withExposedPorts(3025);
-
-    @DynamicPropertySource
-    static void configureMailHost(DynamicPropertyRegistry registry) {
-        registry.add("spring.mail.host", greenMailGenericContainer::getHost);
-        registry.add("spring.mail.port", greenMailGenericContainer::getFirstMappedPort);
-    }
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+            .withConfiguration(GreenMailConfiguration.aConfig().withUser("user", "admin"))
+            .withPerMethodLifecycle(false);
 
     @LocalServerPort
     int port;
