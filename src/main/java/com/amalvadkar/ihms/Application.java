e080a4f2f95c2dd6f56029c434648b49105e103d
Abhishek <60572185+abhishekmalvadkar@users.noreply.github.com>
2024-06-08 16:16:48 +0530

    Update Application.java

--- src/main/java/com/amalvadkar/ihms/Application.java
+++ src/main/java/com/amalvadkar/ihms/Application.java
@@ -4,22 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
-import java.util.Random;
 
 @SpringBootApplication
 @EnableAsync
 @EnableConfigurationProperties(ApplicationProperties.class)
 public class Application {
 	public static void main(String[] args) {
-		String username = "admin";
-		String password = "test";
-		System.out.println(password);
-		SpringApplication.run(Application.class, args);
-	}
-
-	public String random() {
-        // Insecure random number generation
-        Random random = new Random();
-        return "Random number: " + random.nextInt();
+	     SpringApplication.run(Application.class, args);
 	}
 }
