460421b7690f28ec156dbd84e926aa25a42f91d3
Abhishek <60572185+abhishekmalvadkar@users.noreply.github.com>
2024-06-09 17:29:59 +0530

    Update Application.java

--- src/main/java/com/amalvadkar/ihms/Application.java
+++ src/main/java/com/amalvadkar/ihms/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
-import java.sql.*;
+
 
 @SpringBootApplication
 @EnableAsync
@@ -13,34 +13,5 @@ public class Application {
 	public static void main(String[] args) {
 	     SpringApplication.run(Application.class, args);
 	}
-
-	
-
-
-    public static void testCodeQL() {
-        // Vulnerable code: concatenating user input into SQL query
-        String username = "admin";
-        String password = "test";
-        
-        try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase", "root", "password");
-            Statement stmt = conn.createStatement();
-            
-            String query = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "'";
-            ResultSet rs = stmt.executeQuery(query); // Vulnerable to SQL injection
-            
-            // Process result set
-            while (rs.next()) {
-                // Do something with the results
-            }
-            
-            // Close resources
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
 }
