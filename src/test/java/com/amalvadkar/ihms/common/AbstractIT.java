e8f16e30ca3fd9cb3b8773cff61df55fe1029b5c
Abhishek Malvadkar <abhi@dev.com>
2024-06-16 13:48:06 +0530

    GH-98 : Just refactoring

--- src/test/java/com/amalvadkar/ihms/common/AbstractIT.java
+++ src/test/java/com/amalvadkar/ihms/common/AbstractIT.java
@@ -4,6 +4,7 @@ import com.amalvadkar.ihms.TestConfig;
 import com.icegreen.greenmail.configuration.GreenMailConfiguration;
 import com.icegreen.greenmail.junit5.GreenMailExtension;
 import com.icegreen.greenmail.util.ServerSetupTest;
+import com.redis.testcontainers.RedisContainer;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -12,6 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -26,12 +31,24 @@ public abstract class AbstractIT {
             .withConfiguration(GreenMailConfiguration.aConfig().withUser("user", "admin"))
             .withPerMethodLifecycle(false);
 
+    @Container
+    private static final RedisContainer REDIS_CONTAINER =
+            new RedisContainer(DockerImageName.parse("redis:7")).withExposedPorts(6379);
+
     @LocalServerPort
     int port;
+
+    @DynamicPropertySource
+    private static void registerRedisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", REDIS_CONTAINER::getHost);
+        registry.add("spring.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379).toString());
+    }
+
 
     @BeforeEach
     void setUp(){
         RestAssured.port = port;
     }
+
 
 }
