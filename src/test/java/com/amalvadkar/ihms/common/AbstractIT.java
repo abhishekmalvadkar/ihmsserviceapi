961bb8c0554d5f52f08d324734704cfad92a28b5
Abhishek Malvadkar <abhi@dev.com>
2024-06-16 17:16:28 +0530

    GH-98 : Just refactoring

--- src/test/java/com/amalvadkar/ihms/common/AbstractIT.java
+++ src/test/java/com/amalvadkar/ihms/common/AbstractIT.java
@@ -38,8 +38,8 @@ public abstract class AbstractIT {
                 .withExposedPorts(6379);
         redis.start();
         System.out.println("Fetch settings");
-        System.setProperty("spring.redis.host", redis.getHost());
-        System.setProperty("spring.redis.port", redis.getMappedPort(6379).toString());
+        System.setProperty("spring.data.redis.host", redis.getHost());
+        System.setProperty("spring.data.redis.host", redis.getMappedPort(6379).toString());
         System.out.println("The local port mapped to 6379 is " + redis.getMappedPort(6379).toString());
     }
 
