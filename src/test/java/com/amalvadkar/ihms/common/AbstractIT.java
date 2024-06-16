e29d3c71fb6f16743caff59f6a49e76e771789ea
Abhishek Malvadkar <abhi@dev.com>
2024-06-16 14:08:19 +0530

    GH-98 : Just refactoring

--- src/test/java/com/amalvadkar/ihms/common/AbstractIT.java
+++ src/test/java/com/amalvadkar/ihms/common/AbstractIT.java
@@ -4,8 +4,8 @@ import com.amalvadkar.ihms.TestConfig;
 import com.icegreen.greenmail.configuration.GreenMailConfiguration;
 import com.icegreen.greenmail.junit5.GreenMailExtension;
 import com.icegreen.greenmail.util.ServerSetupTest;
+import com.redis.testcontainers.RedisContainer;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -13,7 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -31,14 +30,14 @@ public abstract class AbstractIT {
 
     // Define the Redis container
     @Container
-    public static GenericContainer<?> redis = new GenericContainer<>("redis:6.0.9")
-            .withExposedPorts(6379);
+    private static final RedisContainer container = new RedisContainer(
+            RedisContainer.DEFAULT_IMAGE_NAME.withTag(RedisContainer.DEFAULT_TAG));
 
-    // Initialize Redis connection properties
-    @BeforeAll
-    public static void setUpBeforeAll() {
-        System.setProperty("spring.redis.host", redis.getHost());
-        System.setProperty("spring.redis.port", String.valueOf(redis.getFirstMappedPort()));
+
+
+    static {
+        System.setProperty("spring.redis.host", container.getHost());
+        System.setProperty("spring.redis.port", String.valueOf(container.getFirstMappedPort()));
     }
     @LocalServerPort
     int port;
