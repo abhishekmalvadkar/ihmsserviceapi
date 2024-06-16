d366220262057e99b77afbf42f1d3b48a17ffc19
Abhishek Malvadkar <abhi@dev.com>
2024-06-16 13:28:59 +0530

    GH-98 : Just refactoring

--- src/test/java/com/amalvadkar/ihms/common/ContainersConfig.java
+++ src/test/java/com/amalvadkar/ihms/common/ContainersConfig.java
@@ -3,6 +3,7 @@ package com.amalvadkar.ihms.common;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -13,5 +14,12 @@ public class ContainersConfig {
     MySQLContainer<?> mysqlContainer(){
         return new MySQLContainer<>("mysql:8.0-debian");
     }
+
+    @Bean
+    @ServiceConnection(name = "redis")
+    public GenericContainer<?> redisContainer() {
+        return new GenericContainer<>("redis:7");
+    }
+
 
 }
